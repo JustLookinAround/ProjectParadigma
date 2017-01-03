272bcfc95013bf85b9ed7ed185ce6a87d70daec4
justlookinaround <cooljayz@gmail.com>
2017-01-04 00:28:51 +0700

    add institution

--- app/src/main/java/com/sourcey/materiallogindemo/MainMenuActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/MainMenuActivity.java
@@ -1,8 +1,10 @@
 package com.sourcey.materiallogindemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
+
+    public void goToIns(View view){
+        Intent intent = new Intent(this, institutionActivity.class);
+        startActivity(intent);
+        finish();
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
 }
